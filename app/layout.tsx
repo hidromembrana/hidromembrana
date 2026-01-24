0fdaad52519b95ef1cd03191cd0ce7d62fa9e861
TEO <alfredo.ssm@gmail.com>
2026-01-24 17:25:09 -0300

    og imagen

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export const metadata: Metadata = {
     siteName: 'Hidromembrana',
     images: [
       {
-        url: "/logo.png",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: "Hidromembrana - Soluciones en Geosintéticos",
